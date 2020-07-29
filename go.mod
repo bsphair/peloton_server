f4c53ec167e6e9640963dd88366b77db108390ac
Brian Phair <bsphair@gmail.com>
2020-07-29 08:13:25 -0400

    Added Login

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/bsphair/peloton_server.git
 
 go 1.14
+
+require (
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
+)
--- internal/main.go
+++ internal/main.go
@@ -7,6 +7,10 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/handlers"
+
+	"github.com/gorilla/mux"
 )
 
 /*
@@ -22,14 +26,28 @@ https://api.pelotoncycle.com/api/workout/{workoutID}?joins=peloton,peloton.ride,
 https://api.pelotoncycle.com/api/workout/{workoutID}/sample?every_n=10&fields=seconds_since_pedaling_start,power,cadence,speed,heart_rate,distance&limit=14400
 */
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
+
+	enableCors(&w)
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	name := r.FormValue("username_or_email")
+	fmt.Println(name)
 
 	// Read the request body to get the username and password
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Error reading body: %v", err)
 	}
-
+	fmt.Println(bytes.NewBuffer(body))
 	// Make http request to login and get basic data
 	resp, err := http.Post("https://api.onepeloton.com/auth/login", "application/json", bytes.NewBuffer(body))
 
@@ -44,6 +62,8 @@ func login(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
+	fmt.Println(string(data))
+
 	json.NewEncoder(w).Encode(string(data))
 }
 
@@ -51,14 +71,18 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the homepage")
 }
 
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-
-	http.HandleFunc("/login", login)
-
-	log.Fatal(http.ListenAndServe(":10000", nil))
-}
-
 func main() {
-	handleRequests()
+	router := mux.NewRouter()
+
+	headers := handlers.AllowedHeaders([]string{"Content-Type", "X-Requested-With", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET", "POST"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+
+	router.HandleFunc("/login", login).Methods("POST")
+
+	// http.HandleFunc("/", homePage)
+
+	// http.HandleFunc("/login", login)
+
+	log.Fatal(http.ListenAndServe(":10000", handlers.CORS(headers, methods, origins)(router)))
 }
